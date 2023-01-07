4cf83972de678572851021354b6a520614f30d01
Febrian <febrian.evolution@gmail.com>
2023-01-07 22:56:35 +0700

    Add Auth Controller for and take action for registration user

---
+++ RefreshToken/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RefreshToken.Dto;
+using RefreshToken.Model;
+
+namespace RefreshToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        //registering for user
+        [HttpPost]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            var user = new User { UserName = request.UserName };
+            return Ok(user);
+        }
+    }
+}
