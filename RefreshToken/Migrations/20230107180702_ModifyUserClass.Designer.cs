1a2039ebd2bf399da50a941efae255cd95fa450b
Febrian <febrian.evolution@gmail.com>
2023-01-08 01:08:02 +0700

    Modify User Class to add RefreshToken, TokenCreated and Expires

--- RefreshToken/Migrations/20230107180702_ModifyUserClass.Designer.cs
+++ RefreshToken/Migrations/20230107180702_ModifyUserClass.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RefreshToken.Data;
 
@@ -11,9 +12,10 @@ using RefreshToken.Data;
 namespace RefreshToken.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230107180702_ModifyUserClass")]
+    partial class ModifyUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RefreshToken/Model/User.cs
+++ RefreshToken/Model/User.cs
@@ -9,6 +9,8 @@ namespace RefreshToken.Model
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; } = new byte[32];
         public byte[] PasswordSalt { get; set; } = new byte[32];
-
+        public string RefreshToken { get; set; }
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
     }
 }
