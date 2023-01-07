8192c999e623a6e1ed4f9fa97e6745d7a0cee093
Febrian <febrian.evolution@gmail.com>
2023-01-07 22:47:17 +0700

    Add User Model

---
+++ RefreshToken/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace RefreshToken.Model
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+
+    }
+}
